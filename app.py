a26931a1aead0ec7c9945663f1b9ddb3a1ba8848
Amade Marques Sabite <amademarquessabite0@gmail.com>
2026-02-20 09:15:06 +0200

    Recriar app.py com correção da porta para Render

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask, render_template
 import os
 
-# Cria o app Flask
 app = Flask(__name__)
 
-# Rota principal
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Inicia o servidor
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
-    )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
